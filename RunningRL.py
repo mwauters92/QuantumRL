e1911921ff565505cbb06525abb979abc7b882aa
mwauters92 <43782117+mwauters92@users.noreply.github.com>
2019-11-07 14:10:24 +0100

    Add files via upload
    Complete rewriting of quantum_env.py. Now there is a father class containing the methods for the RL algorithm and children classes that implement the specific models. RunningRL.py has been modified accordingly

--- RunningRL.py
+++ RunningRL.py
@@ -4,45 +4,29 @@ import quantum_env as qenv
 from spinup import ppo
 import tensorflow as tf
 
-actType= 'pspin'   #bin or cont
-tau=np.pi/4
-P=np.concatenate((np.arange(2,20,2),np.arange(20,64,4)))
-P=np.arange(1,18,1)
-#P=[1]
+
+actType= 'cont'   #bin or cont
+model='pSpin' 
+tau=np.pi/4 # total time used in the binary action process
+P=np.arange(1,3,1)
 rew ='energy'  # energy, logE, expE
-epochs=1000
+epochs=100
+#paramenters related to the pspin model (for the moment)
+N=32
+ps=2
 
-if actType == 'bin':
-  for Nt in P:
-    dt = tau/Nt
-    tf.reset_default_graph()    
-    env_fn = lambda : qenv.Spin_bin(Nt,dt,rtype=rew,noise=0.2)
-    ac_kwargs = dict(hidden_sizes=[16,16], activation=tf.nn.relu)
-    dirOut=actType+"P"+str(Nt)+"_tau025pi_rw"+rew+'_noise0.2'
-    logger_kwargs = dict(output_dir=dirOut, exp_name='RL_first_try')
-    ppo(env_fn=env_fn, ac_kwargs=ac_kwargs, steps_per_epoch=1000, epochs=epochs, logger_kwargs=logger_kwargs, gamma=1.0)
+for Nt in P:
+  dt = tau/Nt
+  tf.reset_default_graph() 
+  if model == 'SingleSpin':   
+    env_fn = lambda : qenv.SingleSpin(Nt,rew,dt,actType)
+  elif model == 'pSpin':
+    env_fn = lambda : qenv.pSpin(N,ps,Nt,rew,dt,actType)
+  else:
+    raise ValueError(f'Invalid model:{model}')
 
-#actType='cont'
-if actType == 'cont':
-  for Nt in P:
-    tf.reset_default_graph()    
-    env_fn = lambda : qenv.Spin_cont(Nt,rtype=rew,noise=1.0)
-    #env_fn = lambda : qc.Quantum(Nt,rtype=rew)
-    ac_kwargs = dict(hidden_sizes=[16,16], activation=tf.nn.relu)
-    dirOut=actType+"P"+str(Nt)+"_rw"+rew+'_noise1.0'
-    logger_kwargs = dict(output_dir=dirOut, exp_name='RL_first_try')
-    ppo(env_fn=env_fn, ac_kwargs=ac_kwargs, steps_per_epoch=1000, epochs=epochs, logger_kwargs=logger_kwargs, gamma=1.0)
-
-
-
-if actType == 'pspin':
-  N=32
-  for Nt in P:
-    tf.reset_default_graph()    
-    env_fn = lambda : qenv.Pspin_cont(N,2,Nt,rtype=rew)
-    #env_fn = lambda : qc.Quantum(Nt,rtype=rew)
-    ac_kwargs = dict(hidden_sizes=[16,16], activation=tf.nn.relu)
-    dirOut=actType+"P"+str(Nt)+"_N"+str(N)+"_rw"+rew
-    logger_kwargs = dict(output_dir=dirOut, exp_name='RL_first_try')
-    ppo(env_fn=env_fn, ac_kwargs=ac_kwargs, steps_per_epoch=1000, epochs=epochs, logger_kwargs=logger_kwargs, gamma=1.0)
+  ac_kwargs = dict(hidden_sizes=[16,16], activation=tf.nn.relu)
+  dirOut=model+actType+"P"+str(Nt)+"_tau025pi_rw"+rew
+  logger_kwargs = dict(output_dir=dirOut, exp_name='RL_first_try')
+  ppo(env_fn=env_fn, ac_kwargs=ac_kwargs, steps_per_epoch=1000, epochs=epochs, logger_kwargs=logger_kwargs, gamma=1.0)
 
--- quantum_env.py
+++ quantum_env.py
@@ -39,14 +39,37 @@ SIGMA_X = np.array([[0, 1],[1, 0]])
 SIGMA_Y = np.array([[0, -1j],[1j, 0]])
 SIGMA_Z = np.array([[1, 0],[0, -1]])
 
-# ------------------------------------
-# class Spin_cont
-# ------------------------------------
+#--------------------------------------
+# General quantum enviroment for Reinforcement Learning
+#--------------------------------------
+class QuantumEnviroment():
+    '''
+    General class that is passed to Spinningup reinforcment learning routines
+    Variables:
+        P (int): number of steps in one episode
+        rtype (string): type of reward computed from the system energy E at the end of the episode 
+            - energy: the reward is -E 
+            - expE: reward is exp(-2*E)
+            - logE: reward is -log( 1 + E + 1.e-8) this can be used only if the minimum energy (rescaled) is known 
+        dt (float32): evolution time at each step of an episode. It is used onyly if the action in binary
+        acttype (string): set the type of actions that can be performed on the system
+            - bin: at each step the system evolve with H1 or H2 ONLY, for a time step dt
+            - cont: at each step the system evolves with both H1 and H2; the action consits in choosing the two time steps
+        g_target (float32): transverse field in the target Hamiltonian H_target = H1 + g_target * H2. Default is 0
+        noise (float32): noise added to the initial state at the beginning of each episode. Default is 0
+    
 
-
-class Spin_cont():
-
-    def __init__(self, P,rtype,  g_target = 0, noise=0, Hx = -np.copy(SIGMA_X), Hz = -np.copy(SIGMA_Z)):
+    Methods:
+        __init__: constructor
+        reset: reset the state to inital one + noise
+        get_observable: from the quantum state gets the observable passed to the RL algorithm
+        get_dense_Uevol: perform quantum unitary evolution on the state  
+        get_instantaneous_reward: computes the reard given the state and the step number inside an episode
+        step: perform a single action on the state
+        render: does nothing but it is required by the RL algorithm
+    Please notice that all methods but get_dense_Uevol are necessary for spinningup
+    '''
+    def __init__(self, P, rtype, dt, acttype, g_target = 0, noise=0, Hx = None, Hz = None):
 
         # here all variables for you
         self.state = None
@@ -54,156 +77,92 @@ class Spin_cont():
         self.P = P
         self.rtype=rtype
         self.noise=noise
-        self.H1 = Hz
-        self.H2 = Hx
-        self.H_target = Hz + g_target * Hx
-        self.H_start = Hx
-        self.Hdim = Hz.shape[0]
-
-        self.observation_space = spaces.Box(low=-1.0, high=1.0, shape=(4,), dtype=np.float32)
-        self.action_space = spaces.Box(low=0., high=2*np.pi, shape=(2,), dtype=np.float32)
-
-        # get initial state
-        E0, eigstates0 = eigh(self.H_start)            
-        self.psi_start = eigstates0[:,0]
-
-
-# --------------------------
-
-# FUNCTION THAT I NEED FOR RL
-
-    def description_of_state(self, state):
-        state_real = np.real(state)
-        state_imag = np.imag(state)
-        obs = np.concatenate((state_real, state_imag))
-        return obs
- 
-
-    def get_dense_Uevol(self, H, dt):
-        E, U = eigh(H)
-        exp_E = np.exp(-1j * E * dt)
-        exp_H = np.dot(U, np.multiply(exp_E[:,None], U.conj().T))
-        return exp_H
-
-
-    def reset(self):
-        # initialize a new simulation:
-        # I will call it after receiving a flag "done"
-        self.m = 0
-        self.state = np.copy(self.psi_start)+self.noise*np.random.rand(self.Hdim)
-        self.state/=np.sqrt(np.vdot(self.state,self.state))
-        obs = self.description_of_state(self.state)
-        return obs #not inter. Now gives back reward = 0
-
-
-    def get_instantaneous_reward(self, state, m, P, rtype, a):   # a is action as given in step [0, 2pi]
-        assert m <= P
-        if m == P:
-            E = np.real(np.dot(self.state.T.conj(), np.dot(self.H_target, self.state)))
-            if rtype == "energy":
-                reward = - E
-            elif rtype == "logE":
-                reward = -np.log( 1.e0 - reward + 1.e-8)
-            elif rtype == "expE":
-                reward = np.exp(-2*E)
-            else:
-                reward=0
-                print('wrong rtype')
-            #momo.write(str(-E)+'\n')
-        else: 
-            reward =  0 #(0-np.any(a>0))*0.01
-        return reward
-
-
-
-    def step(self, action):
-        # here I want the quantum evolution for 1 step, 
-        # given the current quantum internal state self.state and the action in input "action"
-        
-        action = np.clip(action, 0, 2*np.pi)
- 
-        # apply the Hamiltonian evolution
-
-        U = self.get_dense_Uevol(self.H1, action[0])
-        self.state = np.dot(U, self.state)
-        U = self.get_dense_Uevol(self.H2, action[1])
-        self.state = np.dot(U, self.state)
-        obs = self.description_of_state(self.state)
-        self.m += 1
-        if self.m == self.P: done = True
-        else: done = False
-        rewards = self.get_instantaneous_reward(self.state, self.m, self.P, self.rtype, action)
-        return np.array(obs), rewards, done, {}
-
-    def close(self):
-        pass
-
-    def render(self):
-        pass  
-
-
-# ------------------------------------
-# End of class Spin_cont
-# ------------------------------------
-
-
-class Spin_bin():
-
-    def __init__(self, P, dt, rtype, g_target = 0, noise=0, Hx = -np.copy(SIGMA_X), Hz = -np.copy(SIGMA_Z)):
-
-        # here all variables for you
-        self.state = None
-        self.m = 0
-        self.P = P
         self.dt = dt
-        self.rtype=rtype
-        self.noise=noise
+        self.acttype = acttype
+
         self.H1 = Hz
         self.H2 = Hx
         self.H_target = Hz + g_target * Hx
         self.H_start = Hx
-        self.Hdim = Hz.shape[0]
-        self.observation_space = spaces.Box(low=-1.0, high=1.0, shape=(4,), dtype=np.float32)
-        self.action_space = spaces.Discrete(2)
 
+        self.Hdim = Hz.shape[0]
+        self.obs_shape = (2*self.Hdim,)  
+        self.observation_space = spaces.Box(low=-1.0, high=1.0, shape=self.obs_shape, dtype=np.float32)
+        ## IF CYCLE TO SET ACTION TYPE
+        if self.acttype == 'bin':
+            self.action_space = spaces.Discrete(2) 
+        elif self.acttype == 'cont':
+            # TODO issue with action space
+            self.action_space = spaces.Box(low= 0., high=2*np.pi, shape=(2,), dtype=np.float32)
+        else:
+            raise ValueError(f'Wrong action type:{self.acttype} not valid')
+        # the action space has to be defined somewhere !!!
+        
         # get initial state
         E0, eigstates0 = eigh(self.H_start)            
         self.psi_start = eigstates0[:,0]
-        #self.psi_start = np.array([1, 0])
+        self.E1, self.U1 = eigh(self.H1)
+        self.E2, self.U2 = eigh(self.H2)
 
+    # FUNCTION THAT I NEED FOR RL
 
-
-# --------------------------
-
-# FUNCTION THAT I NEED FOR RL
-
-    def description_of_state(self, state):
+    def get_observable(self, state):
+    '''
+       It computes the observable given to the RL algorithm. 
+       In the actual implementation it is just the full state stored in 
+       a real vector.
+       Parameters:
+           state (complex): complex vector of dmnesion self.Hdim describing the system state
+       Returns:
+          obs (real): 2*Hdim long real vector (real(state),imag(state)) 
+    '''
         state_real = np.real(state)
         state_imag = np.imag(state)
         obs = np.concatenate((state_real, state_imag))
         return obs
 
- 
 
-    def get_dense_Uevol(self, H, dt):
-        E, U = eigh(H)
+    def get_dense_Uevol(self, E, U, dt):
+    '''
+        Computes the unitary time evolution operator
+        Parameters:
+            E (real): eigenvalues of of the Hamniltonian that enters the evolution operator
+            U (complex): matrix with the eigenvectors of the hamiltonian
+            dt (real): time step
+        Returns:
+            exp_H (complex): time evolution operator
+    '''
         exp_E = np.exp(-1j * E * dt)
         exp_H = np.dot(U, np.multiply(exp_E[:,None], U.conj().T))
         return exp_H
 
 
-
     def reset(self):
+    '''
+        Reset the state to the initial one, with the possible addition of disorder, set by self.noise
+        Returns: 
+            obs (real): observable of reset state
+    '''
         # initialize a new simulation:
         # I will call it after receiving a flag "done"
         self.m = 0
         self.state = np.copy(self.psi_start)+self.noise*np.random.rand(self.Hdim)
         self.state/=np.sqrt(np.vdot(self.state,self.state))
-        obs = self.description_of_state(self.state)
+        obs = self.get_observable(self.state)
         return obs #not inter. Now gives back reward = 0
 
 
     def get_instantaneous_reward(self, state, m, P,rtype):
+        '''
+        From the state computes the instantaneous reward
+        Parameters:
+            state (complex): system state
+            m (int): step during the episode
+            P (int): length of the episode
+            rtype (string): sets the type of reward given (energy,logE,expE)
+        Returns:
+            reward (real): the reawrd is given only at the end of the episode. It is a function of the system energy at the end of the process. 
+        '''
         assert m <= P
         if m == P:
             E = np.real(np.dot(self.state.T.conj(), np.dot(self.H_target, self.state)))
@@ -215,29 +174,52 @@ class Spin_bin():
                 reward = np.exp(-2*E)
             else: 
                 reward = 0
-                print("wrong rtype")
+                raise ValueError(f"wrong reward type:{rtype} is not a valid rtype")
         else: reward = 0
         return reward
 
 
-
     def step(self, action):
+    '''
+         Update the state according to the action chosen by the RL algorithm
+         Parameters:
+             action (real): action to be performed on the system. depends on actType
+         Returns:
+             obs (real): updated observable of the state
+             reward (real): reward of the updated state
+             done (bool): if True the episode is over.
+    '''
         # here I want the quantum evolution for 1 step, 
         # given the current quantum internal state self.state and the action in input "action"
         # action == 0 -> evolve with H1
         # action == 1 -> evolve with H2
+        if self.acttype=='bin':
+            #assert action in [0, 1]
 
-        assert action in [0, 1]
+            # select the correct Hamiltonian
+            if action == 0: 
+                U = self.get_dense_Uevol(self.E1, self.U1, self.dt)
+            elif action >= 1: 
+                U = self.get_dense_Uevol(self.E2, self.U2, self.dt)
+      
+            # apply the Hamiltonian evolution
+            self.state = np.dot(U, self.state)
+        elif self.acttype=='cont':  
+            # continuous action below
+            action = np.clip(action, 0, np.pi*self.Hdim)
+ 
+            # apply the Hamiltonian evolution
 
-        # select the correct Hamiltonian
-        if action == 0: H = self.H1
-        elif action == 1: H = self.H2
+            U = self.get_dense_Uevol(self.E1, self.U1, action[0])
+            self.state = np.dot(U, self.state)
+            U = self.get_dense_Uevol(self.E2, self.U2, action[1])
+            self.state = np.dot(U, self.state)
+ 
+        else:
+            raise ValueError(f'Wrong action type:{self.acttype} not valid')
 
-        # apply the Hamiltonian evolution
-
-        U = self.get_dense_Uevol(H, self.dt)
-        self.state = np.dot(U, self.state)
-        obs = self.description_of_state(self.state)
+        # this part is the same for both binary and blabla
+        obs = self.get_observable(self.state)
         self.m += 1
         if self.m == self.P: done = True
         else: done = False
@@ -250,52 +232,54 @@ class Spin_bin():
     def render(self):
         pass  
 
-
-
 # ------------------------------------
-# End of class Spin_bin
+# End of class QuantumEnviroment
 # ------------------------------------
 
-class Pspin_cont():
 
-    def __init__(self, N, p, P,rtype,  g_target = 0, noise=0):
+#-------------------------------------
+# Model class for pSpin model
+#-------------------------------------
 
-        # here all variables for you
-        self.state = None
-        self.m = 0
-        self.N = N
-        self.p = p
-        self.P = P
-        self.rtype=rtype
-        self.noise=noise
-        self.Hz = self.pspinHz(N,p)
-        self.Hx , self.Ux = self.S_x(N,1)
-        self.H_target = self.Hz - g_target * self.Hx
-        self.Hdim = N+1
+class pSpin(QuantumEnviroment):
+    '''Children class of QuantumEnviroment. Add specific model (pspin) to the class.
+       Paramenters:
+           N (int): number of spin variables
+           ps (int): rank of the interaction
+       
+       Methods:
+           pspinHz: construct the z-part of the Hamiltonian (diagonal)
+           xSpin: contruct the  x-component of the global spin operator 
+    '''
 
-        self.observation_space = spaces.Box(low=-1.0, high=1.0, shape=(2*self.Hdim,), dtype=np.float32)
-        self.action_space = spaces.Box(low=0., high=2*np.pi, shape=(2,), dtype=np.float32)
-
-        # get initial state
-        E0, eigstates0 = eigh(-self.Hx)            
-        self.psi_start = eigstates0[:,0]
-
-
-# --------------------------
-
-# FUNCTION THAT I NEED FOR RL
-
+    def __init__(self, N, ps, P, rtype, dt, acttype, g_target = 0, noise=0):
+        
+        Hx = -self.xSpin(N,0)
+        Hz = self.pspinHz(N,ps)
+        QuantumEnviroment.__init__(self, P, rtype, dt, acttype, g_target = 0, noise=0, Hx = Hx, Hz = Hz)
+   
     def pspinHz(self,N,p):
-        """Construct the x and z Hamiltonian fo the pspin model"""
+        """Construct the x and z Hamiltonian fo the pspin model
+           Parameters:
+               N (int): chain length
+               p (int): rank ofthe interaction
+           Returns:
+               Hz (real): interaction part of the Hamiltonian of the pSpin model
+        """
         Hz=np.zeros([N+1,N+1])
         x=np.linspace(-1,1,N+1,endpoint=True);
         Hz=-N*np.diag(x**p);
         return Hz
 
-    def S_x(self,N,job):
+    def xSpin(self,N,job):
         """Transverse field S_x in the z basis representation.
-        if job==1 it provides also the unitary matrix that 
-        diagonalizes S_x"""
+           Parameters:
+               N (int): chain length
+               job (int): 0 or 1 tells if to diagonalize Sx
+           Returns:
+               Sx (real): x component of the total spin
+               Ux (complex): if job=1 Ux contains the eigenvectors of Sx
+        """
         Sx=np.zeros([N+1,N+1])
         x=np.linspace(-1,1,N+1);
         for j in range(N):
@@ -306,81 +290,26 @@ class Pspin_cont():
             return Sx, Usx
         elif(job==0):
             return(Sx)
+#------------------------------------------------------------
+# End of pSpin class
+#-----------------------------------------------------------
 
+#-------------------------------------
+# Model class for single spin 1/2
+#-------------------------------------
 
-    def description_of_state(self, state):
-        state_real = np.real(state)
-        state_imag = np.imag(state)
-        obs = np.concatenate((state_real, state_imag))
-        return obs
- 
+class SingleSpin(QuantumEnviroment):
+    '''Children class of QuantumEnviroment. Add specific model (single spin 1/2) to the class.
+       It passes to QuantumEnviroment only the pauli matrices
+    '''
 
-    def get_dense_UevolZ(self, H, dt):
-        exp_H = np.exp(-1j * H.diagonal() * dt)
-        return np.diag(exp_H)
-
-    def get_dense_UevolX(self, U,dt):
-        N=self.Hdim-1
-        exp_E=np.exp(1j*dt*np.linspace(-N,N,N+1))
-        exp_H = np.dot(U, np.multiply(exp_E[:,None], U.conj().T))
-        return exp_H
-
-    def reset(self):
-        # initialize a new simulation:
-        # I will call it after receiving a flag "done"
-        self.m = 0
-        self.state = np.copy(self.psi_start)+self.noise*np.random.rand(self.Hdim)
-        self.state/=np.sqrt(np.vdot(self.state,self.state))
-        obs = self.description_of_state(self.state)
-        return obs #not inter. Now gives back reward = 0
-
-
-    def get_instantaneous_reward(self, state, m, P, rtype, a):   # a is action as given in step [0, 2pi]
-        assert m <= P
-        if m == P:
-            E = np.real(np.dot(self.state.T.conj(), np.dot(self.H_target, self.state)))
-            if rtype == "energy":
-                reward = - E
-            elif rtype == "logE":
-                reward = -np.log( 1.e0 - reward + 1.e-8)
-            elif rtype == "expE":
-                reward = np.exp(-2*E)
-            else:
-                reward=0
-                print('wrong rtype')
-            #momo.write(str(-E)+'\n')
-        else: 
-            reward =  0 #(0-np.any(a>0))*0.01
-        return reward
-
-
-
-    def step(self, action):
-        # here I want the quantum evolution for 1 step, 
-        # given the current quantum internal state self.state and the action in input "action"
+    def __init__(self, P, rtype, dt, acttype, g_target = 0, noise=0):
         
-        action = np.clip(action, 0, np.pi*self.Hdim)
- 
-        # apply the Hamiltonian evolution
+        QuantumEnviroment.__init__(self, P, rtype, dt, acttype, g_target = 0, noise=0, Hx = -np.copy(SIGMA_X), Hz = -np.copy(SIGMA_Z) )
+   
 
-        U = self.get_dense_UevolZ(self.Hz, action[0])
-        self.state = np.dot(U, self.state)
-        U = self.get_dense_UevolX(self.Ux, action[1])
-        self.state = np.dot(U, self.state)
-        obs = self.description_of_state(self.state)
-        self.m += 1
-        if self.m == self.P: done = True
-        else: done = False
-        rewards = self.get_instantaneous_reward(self.state, self.m, self.P, self.rtype, action)
-        return np.array(obs), rewards, done, {}
-
-    def close(self):
-        pass
-
-    def render(self):
-        pass  
-
-
+#---------------------------------------
+# end of class SingleSpin
 # ------------------------------------
-# End of class Pspin_cont
-# ------------------------------------
+
+
