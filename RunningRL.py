3755bae9ed8cbeed48ccc6556665f51756b0c437
mwauters92 <mwauters@zagor.phys.sissa.it>
2019-11-27 17:23:00 +0100

    Added TFIM model to the code

--- RunningRL.py
+++ RunningRL.py
@@ -5,29 +5,38 @@ from spinup import ppo
 import tensorflow as tf
 
 
-actType= 'cont'   #bin or cont
-model='pSpin' 
-tau=np.pi/4 # total time used in the binary action process
-P=np.arange(18,19,1)
-rew ='energy'  # energy, logE, expE
-epochs=1000
-#paramenters related to the pspin model (for the moment)
-N=32
-ps=2
+actType= 'cont'                 # action type: bin, cont
+model='pSpin'                   # model : pSpin, SingleSpin, TFIM
+tau=20                          # total time used in the binary action process
+P=np.arange(2,19,2)             # list of episode lenghts 
+P=[8]
+rtype ='energy'                 #reward types: energy, logE, expE
+epochs=256                      # number of epochs
+nstep=8192                      # steps per episodes
 
+# physical parameters
+N=np.array([16,32,64,128])      # list of sizes
+N=[32]
+ps=2                            # interaction rank of the pSpin model
+
+dirO = "../Output/"+model+"/ep"+str(epochs)+"_sep"+str(nstep)+"/"
 for Nt in P:
   dt = tau/Nt
-  tf.reset_default_graph() 
-  if model == 'SingleSpin':   
-    env_fn = lambda : qenv.SingleSpin(Nt,rew,dt,actType)
-    dirOut='Output/'+model+actType+"P"+str(Nt)+'_rw'+rew
-  elif model == 'pSpin':
-    env_fn = lambda : qenv.pSpin(N,ps,Nt,rew,dt,actType)
-    dirOut='Output/pspin'+"P"+str(Nt)+'_N'+str(N)+'_rw'+rew
-  else:
-    raise ValueError(f'Invalid model:{model}')
-
-  ac_kwargs = dict(hidden_sizes=[16,16], activation=tf.nn.relu)
-  logger_kwargs = dict(output_dir=dirOut, exp_name='RL_first_try')
-  ppo(env_fn=env_fn, ac_kwargs=ac_kwargs, steps_per_epoch=1000, epochs=epochs, logger_kwargs=logger_kwargs, gamma=1.0)
+  for Ns in N:
+    tf.reset_default_graph() 
+    if model == 'SingleSpin':   
+      env_fn = lambda : qenv.SingleSpin(Nt,rtype,dt,actType)
+      dirOut=dirO+model+actType+"P"+str(Nt)+'_rw'+rtype
+    elif model == 'pSpin':
+      env_fn = lambda : qenv.pSpin(Ns,ps,Nt,rtype,dt,actType)
+      dirOut=dirO+'pspin'+"P"+str(Nt)+'_N'+str(Ns)+'_rw'+rtype
+    elif model == 'TFIM':
+      env_fn = lambda : qenv.TFIM(Ns,Nt,rtype,dt,actType)
+      dirOut=dirO+'TFIM'+"P"+str(Nt)+'_N'+str(Ns)+'_rw'+rtype
+    else:
+      raise ValueError(f'Invalid model:{model}')
+    dirOut += '/network32x16'
+    ac_kwargs = dict(hidden_sizes=[32,16], activation=tf.nn.relu)
+    logger_kwargs = dict(output_dir=dirOut, exp_name='RL_first_try')
+    ppo(env_fn=env_fn, ac_kwargs=ac_kwargs, steps_per_epoch=nstep, epochs=epochs, logger_kwargs=logger_kwargs, gamma=1.0)
 
