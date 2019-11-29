3c0b3096d7399d867fe4b763ec932c3e4db0dccb
mwauters92 <mwauters@zagor.phys.sissa.it>
2019-11-29 11:28:24 +0100

    Added testing of trained TFIM model

--- test_quantum.py
+++ test_quantum.py
@@ -9,33 +9,40 @@ import quantum_env as qenv
 from spinup.utils.test_policy import load_policy, run_policy
 
 
-def rew2en(reward, rtype):
+def rew2en(reward, rtype, N=2):
     if rtype == 'energy':
         return -reward
     elif rtype == 'expE':
-        return -0.5*np.log(reward)
+        return -0.25*np.log(reward)*(N-1)
     elif rtype == 'logE':
         return 1.0-np.exp(-reward)
     else:
         return 0
 
 actType= 'cont'   #bin or cont
-model='pSpin'
+model='TFIM'
 tau=2*np.pi
-P=np.arange(1,18,1)
+P=np.arange(2,20,2)
 rew = 'energy'   # energy, logE, expE
 Na=20
 N=32
 ps=2
+epochs=512
+nstep=8192
+
+dirO = "../Output/"+model+"/ep"+str(epochs)+"_sep"+str(nstep)+"/"
 for Nt in P:
     tf.reset_default_graph()
     dt=tau/Nt
     if model == 'SingleSpin':
         env = qenv.SingleSpin(Nt,rew,dt,actType)
-        dirOut='Output/'+model+actType+"P"+str(Nt)+'_rw'+rew
+        dirOut=dirO+model+actType+"P"+str(Nt)+'_rw'+rtype
     elif model == 'pSpin':
         env = qenv.pSpin(N,ps,Nt,rew,dt,actType)
-        dirOut='Output/pspin'+"P"+str(Nt)+'_N'+str(N)+'_rw'+rew
+        dirOut=dirO+'pspin'+"P"+str(Nt)+'_N'+str(Ns)+'_rw'+rtype
+    elif model == 'TFIM':
+        env_fn = lambda : qenv.TFIM(Ns,Nt,rtype,dt,actType)
+        dirOut=dirO+'TFIM'+"P"+str(Nt)+'_N'+str(Ns)+'_rw'+rtype
     else:
         raise ValueError(f'Model not implemented:{model}')
         
@@ -51,8 +58,8 @@ for Nt in P:
             for i in range(Nt):
                 a = get_action(o)
                 o, r, d, _ = env.step(a)
-                data[ep*Nt+i,:]=np.array([ep, a, r, rew2en(r,rew)])
-            summary[ep,:]=np.array([ep,r,rew2en(r,rew)])
+                data[ep*Nt+i,:]=np.array([ep, a, r, rew2en(r,rew,N)])
+            summary[ep,:]=np.array([ep,r,rew2en(r,rew,N)])
         summary[-1,:]=np.mean(summary[:-1,:],axis=0)
 
         np.savetxt(dirOut+"/actions.dat",data, header=head,fmt='%03d  %1d  %.6e  %.6e') 
@@ -64,13 +71,12 @@ for Nt in P:
         data=np.zeros([Na*Nt,5])
         summary=np.zeros([Na+1,4])
         for ep in range(Na):
-            print(env)
             o = env.reset()
             for i in range(Nt):
                 a = np.clip(get_action(o),0,2*np.pi)
                 o, r, d, _ = env.step(a)
-                data[ep*Nt+i,:]=np.array([ep, a[0],a[1], r, rew2en(r,rew)])
-            summary[ep,:]=np.array([ep,r,rew2en(r,rew),np.sum(data[ep*Nt:(ep+1)*Nt,1:2])])
+                data[ep*Nt+i,:]=np.array([ep, a[0],a[1], r, rew2en(r,rew,N)])
+            summary[ep,:]=np.array([ep,r,rew2en(r,rew,N),np.sum(data[ep*Nt:(ep+1)*Nt,1:2])])
         summary[-1,:]=summary[:-1,:].mean(axis=0)
         np.savetxt(dirOut+"/actions.dat",data, header=head,fmt='%03d  %.6e  %.6e  %.6e  %.6e') 
         head='# 1-episode,  2-reward 3-energy, 4-time'
